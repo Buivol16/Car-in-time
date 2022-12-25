f1a989319688f5c59d3c4175c3402fa89d240fc9
Buivol16 <khmaradenis64@gmail.com>
2022-12-25 18:50:55 +0200

    Initial commit where project had started.

---
+++ src/main/java/ua/denis/project/CarInTime/CarInTimeApplication.java
@@ -0,0 +1,13 @@
+package ua.denis.project.CarInTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/denis/project/CarInTime/CarInTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.denis.project.CarInTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
